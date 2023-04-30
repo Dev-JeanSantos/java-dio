ade0fe249b6b0bc1bc53e8740addbc6fa7607d2d
Dev-JeanSantos <jeancbsan@gmail.com>
2023-04-30 10:48:48 -0300

    types of variables

---
+++ types-and-variables/src/TypesVariables.java
@@ -0,0 +1,16 @@
+public class TypesVariables {
+    public static void main(String[] args) throws Exception {
+
+        //Primitive Types
+        //VARIABLES
+        double area = 200.3;
+        int number = 1000;
+        String name = "Jean Santos";
+        boolean positive = true;
+        System.out.println(area +" "+ number+" "+ name +" "+ positive);
+
+        //CONSTANTS
+        final double VALU_OF_PI = 3.1416;
+        System.out.println(VALU_OF_PI);
+    }
+}
