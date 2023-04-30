ce43ede76ec6e9ceb0a914cde7ad11ee055e99ee
Dev-JeanSantos <jeancbsan@gmail.com>
2023-04-30 10:01:24 -0300

    anatomy of classes

---
+++ java-anatomia-classes/src/MinhaVariavel.java
@@ -0,0 +1,24 @@
+public class MinhaVariavel {
+    public static void main(String[] args) {
+
+        //Declarações de variaveis
+        String meuNome = "Jean";
+        int anoFabricacao = 2022;
+        boolean verdadeira = false;
+        anoFabricacao = 2023;
+
+        
+        String firstName = "Jean";
+        String lastName = "Santos";
+        String fullName = fullName(firstName, lastName);
+       
+        System.out.println("The full name: " + fullName);
+    }
+
+    
+        //Criando Metodos
+        public static String fullName( String firstName, String lastName) {
+            return firstName.concat(" ").concat(lastName);
+        }
+    
+}
