757fc25677140c8243ad144fc1c080e077d99adb
Dev-JeanSantos <jeancbsan@gmail.com>
2023-05-23 11:18:23 -0300

    interfaces

---
+++ OrientacaoObjeto/src/Interfaces/Operacaomatematica.java
@@ -0,0 +1,12 @@
+package Interfaces;
+
+interface Operacaomatematica {
+    void soma(double operando1,double operando2 ); 
+
+    void subtracao(double operando1,double operando2 );
+
+    void multiplicacao(double operando1,double operando2 );
+
+    void divisao(double operando1,double operando2 );
+    
+}
